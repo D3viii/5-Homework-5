c97654c62cc22eaa42a3e4bdac035fbb7054bd4e
D3viii <devinsdevs@gmail.com>
2020-03-13 19:41:34 -0400

    Im Lost

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
-$(document).ready({
+var timeBlock = document.getElementsByClassName(".text-block")
 
-    
+timeBlock.textContent = ["1", "2", "3", "4", "5"]
 
-})
+var textArea = document.getElementsByName("textarea")
+textArea.textContent = "hello world"
