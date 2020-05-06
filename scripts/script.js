f1eebfd6256b04b9fa3dc4b326341e66fb0a524b
D3viii <devinsdevs@gmail.com>
2020-05-06 16:53:53 -0400

    Added Date

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,21 @@ $(document).ready(function() {
         } else {
             localStorage.setItem("calendarArray", JSON.stringify(calendarArray))
         }
+        calenderRender()
         
+    }
+
+    init()
+
+    function calenderRender() {
+        for (var i = 9; i < 18; i++) {
+            var timeTXT;
+            if (i === 9) {
+                timeTXT = moment("2013-05-06 0" + i).format("LT")
+            } else {
+                timeTXT = moment("2013-05-06" + i).format("LT")
+            }
+            
+        }
     }
 })
